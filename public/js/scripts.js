dd946af0abfbfc063763609f9619c02102b83f3c
pedrooliver13 <pedro.oliveira@qyon.com>
2024-02-28 21:30:10 -0300

    fix: fix audio

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,9 @@
 import { SlideNav } from "./module/slide.js";
 
 const slide = new SlideNav('[data-slide="slide"]', '[data-slide="container"]');
+const audio = document.querySelector("audio");
+
 slide.init();
 slide.addControls('[data-slide="prev"]', '[data-slide="next"]');
+
+audio.onload(() => audio.play());
