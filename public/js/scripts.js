c113d72edb0bced2b4b01e29a492612bd3e07866
pedrooliver13 <pedro.oliveira@qyon.com>
2024-02-28 23:01:32 -0300

    fix: fix audio

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,13 +2,5 @@ import { SlideNav } from "./module/slide.js";
 
 const slide = new SlideNav('[data-slide="slide"]', '[data-slide="container"]');
 
-const audio = new Audio(
-  "public/audio/Coldplay - Higher Power (Live at The BRIT Awards, London 2021).mp3"
-);
-
 slide.init();
 slide.addControls('[data-slide="prev"]', '[data-slide="next"]');
-
-window.addEventListener("load", () => {
-  audio.play();
-});
