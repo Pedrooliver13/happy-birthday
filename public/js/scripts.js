1d84579e00e9edfb2cdb566854d8ca0a273042bb
pedrooliver13 <pedro.oliveira@qyon.com>
2024-03-03 14:53:47 -0300

    feat: add modal in project

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,5 +2,15 @@ import { SlideNav } from "./module/slide.js";
 
 const slide = new SlideNav('[data-slide="slide"]', '[data-slide="container"]');
 
-slide.init();
-slide.addControls('[data-slide="prev"]', '[data-slide="next"]');
+const buttonInit = document.querySelector("[data-js='init-retro']");
+const modal = document.querySelector("[data-js='modal']");
+const audio = document.querySelector("[data-js='audio']");
+
+const handleClick = () => {
+  slide.init();
+  slide.addControls('[data-slide="prev"]', '[data-slide="next"]');
+  modal.classList.remove("active");
+  audio.play();
+};
+
+buttonInit.addEventListener("click", handleClick);
